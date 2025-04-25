956e149ae97f7a16b3962caa92ac7d28a2285902
Dasuni Anutthara <it22217554@my.sliit.lk>
2025-04-25 10:46:46 +0530

    Refactored controller and added service class for Post CRUD

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./SkillPostSharing/pages/Home.js";
+import LearningPlansDashboard from "./learningplan/learningplan.js";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
       
         <Route path="/" element={<Home />} />
+        <Route path="/plans" element ={<LearningPlansDashboard/>} />
       </Routes>
     </Router>
   );
